8ad21113b727d618fddd84f0cae31e7aa7215f42
ageeha927 <141285328+ageeha927@users.noreply.github.com>
2024-04-29 18:14:21 -0700

    ..

--- js/maze.js
+++ js/maze.js
@@ -1,66 +1,65 @@
 $(document).ready(function() {
-    //Maze Size/Dimensions
-    var mazeHeight = 740
-    var mazeWidth = 740
+    // Maze Size/Dimensions
+    var mazeHeight = 800; // Adjusted maze height
+    var mazeWidth = 800; // Adjusted maze width
 
-    //Player starting position
-    var playerX = 20
-    var playerY = 20
+    // Player starting position
+    var playerX = 40; // Adjusted player starting position
+    var playerY = 40; // Adjusted player starting position
 
-    //Movement Functionality for player
+    // Movement Functionality for player
     function movePlayer(dx, dy) {
         var newX = playerX + dx;
         var newY = playerY + dy;
 
-        //Verify new position is inside of the maze walls
+        // Verify new position is inside of the maze walls
         if (newX >= 0 && newX < mazeWidth && newY >= 0 && newY < mazeHeight) {
-            //Check to see if the new position is a wall or not
+            // Check to see if the new position is a wall or not
             if (!$('#maze').find('.wall').is('[style="top: ' + newY + 'px; left: ' + newX + 'px;"]')) {
-                //We can move the player
+                // We can move the player
                 playerX = newX;
                 playerY = newY;
                 $('#player').css({ top: playerY + 'px', left: playerX + 'px' });
             }
 
-            //Check to see if the player reaches the end
-            if(playerX === 380 && playerY === 380){
-                alert("Congratulations you made it throught maze")
+            // Check to see if the player reaches the end
+            if (playerX === 760 && playerY === 760) {
+                alert("Congratulations you made it through the maze");
             }
-
         }
     }
 
     // Keypress Event Listener
-    $(document).keydown(function(e){
+    $(document).keydown(function(e) {
         switch (e.which) {
             case 37: // Left arrow
-                movePlayer(-20, 0);
+                movePlayer(-40, 0);
                 break;
             case 38: // Up arrow
-                movePlayer(0, -20);
+                movePlayer(0, -40);
                 break;
             case 39: // Right arrow
-                movePlayer(20, 0);
+                movePlayer(40, 0);
                 break;
             case 40: // Down arrow
-                movePlayer(0, 20);
+                movePlayer(0, 40);
                 break;
-            }
-    })
+        }
+    });
 
-    //Create Maze Walls
+    // Create Maze Walls
     var wallCoordinates = [
-        {top:40, left:40},//wall 1
-        {top:20, left:100},//wall 2
-        {top: 20, left: 160},
-        //Add more walls here and in the scss
-    ]
+        { top: 80, left: 80 }, // wall 1
+        { top: 40, left: 200 }, // wall 2
+        { top: 40, left: 320 },
+        // Add more walls here and in the scss
+    ];
 
     for (var i = 0; i < wallCoordinates.length; i++) {
         $('#maze').append('<div class="wall" style="top: ' + wallCoordinates[i].top + 'px; left: ' + wallCoordinates[i].left + 'px;"></div>');
-        }
+    }
 
-    //Create the player and append it to the board
-    $('#maze').append('<div class="player" id="player" style="top: '+ playerY + 'px; left: ' + playerX + 'px;"></div>')
+    // Create the player and append it to the board
+    $('#maze').append('<div class="player" id="player" style="top: ' + playerY + 'px; left: ' + playerX + 'px;"></div>');
 
-})
+});
